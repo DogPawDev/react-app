789de8f559b2a2e43d019c176666303b3809bb8a
dogpaw <foodev@naver.com>
2020-06-15 20:23:35 +0900

    create 구현 : mode 변경 기능

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TOC from "./components/TOC";
 import Content from "./components/Content";
 import Subject from "./components/Subject";
+import Control from './components/Control';
 
 
 
@@ -79,6 +80,12 @@ class App extends Component {
        
        
        ></TOC>
+     
+      <Control onChangeMode={function(_mode){
+        this.setState({
+          mode:_mode
+        });
+      }.bind(this)}></Control>
        <Content title={_title} desc={_desc}></Content>
     </div>
     );
